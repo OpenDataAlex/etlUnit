03a246d7624eae25e853c3a57281d41daabbc5c5
coty <sutherland.coty@gmail.com>
2013-08-10 17:59:56 -0400

    Updated how the yaml is being parsed. Instead of iterating over the array of yaml once its loaded, I am just taking that array in and using it as is.

--- src/com/github/dbaAlex/yaml_reader.py
+++ src/com/github/dbaAlex/yaml_reader.py
@@ -25,25 +25,30 @@ class YAMLReader():
     def readTests(self):
         # this block reads and parses yaml from an individual file
         if self.in_file is not None:
-            test = {}
+            # test = {}
             with open(self.in_file, 'r') as f:
                 prop_list = yaml.load(f.read())
+                self.log.debug(prop_list)
+                return prop_list
 
-                for key, value in prop_list.items():
-                    test[key] = value
+                # TODO: Need to verify that this is what we want. Will this always return a JSON array?
+                # for key, value in prop_list.items():
+                #     test[key] = value
 
-            self.log.debug(test)
-            self.tests.append(test)
+            # self.log.debug(test)
+            # self.tests.append(test)
         # this block reads and parses yaml from multiple files
         elif self.in_dir is not None:
             for test_path in glob.glob('{}/*.yml'.format(self.in_dir)):
-                test = {}
+                # test = {}
                 with open(test_path, 'r') as f:
                     prop_list = yaml.load(f.read())
+                    self.log.debug(prop_list)
+                    self.tests[test_path] = prop_list
 
-                    for key, value in prop_list.items():
-                        test[key] = value
-                self.log.debug(test)
-                self.tests.append(test)
+                    # for key, value in prop_list.items():
+                    #     test[key] = value
+                # self.log.debug(test)
+                # self.tests.append(test)
 
-        return self.tests
+            return self.tests
