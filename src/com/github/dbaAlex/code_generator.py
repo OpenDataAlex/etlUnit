7ea6381432cd6cb07b751a736d8fd78751c250ec
coty <sutherland.coty@gmail.com>
2013-08-10 17:58:53 -0400

    Updated generator to generate differently when a fixture is present. Have not implemented the fixture generation yet, only the testsuite. It seems to be working.

--- src/com/github/dbaAlex/code_generator.py
+++ src/com/github/dbaAlex/code_generator.py
@@ -2,6 +2,7 @@ __author__ = 'coty'
 
 import logging
 from settings import etlunit_config, console
+import os
 
 
 class CodeGenerator():
@@ -10,38 +11,52 @@ class CodeGenerator():
         and generating code from it by filling in templates.
     """
 
-    def __init__(self, out_dir, arr):
+    def __init__(self, out_dir, data):
         self.log = logging.getLogger(name='CodeGenerator')
         self.log.setLevel(etlunit_config['logging_level'])
         self.log.addHandler(console)
 
-        # TODO: Update file extension to '.py'
-        self.file_ext = '.sh'
+        self.file_ext = '.py'
 
-        self.arr = arr
+        self.yaml_data = data
         self.out_dir = out_dir
 
     def generateCode(self):
+        """
+            Generate code has to be smart enough to determine if the json array should generate a test case, or a
+            fixture or if it needs to generate both.
+
+            * If setup or teardown is present, then its a test case
+            * If parent is present, then it needs to extend a fixture
+        """
+        #TODO: Maybe we should have a yaml validation class?
         from jinja2 import Environment, FileSystemLoader
-        import os
+        from time import strftime, gmtime
 
         # TODO: Find a more efficient way to pull in this template other than ../
         out_path = "%s/../../../../templates/" % os.path.dirname(os.path.abspath(__file__))
         j2_env = Environment(loader=FileSystemLoader(out_path), trim_blocks=True)
-        # TODO: Should the template name be dynamic? Will it changed based on the yaml?
-        template_output = j2_env.get_template("%s.tmp" % self.arr[0]['suite']['name'])\
-            .render(connections=self.arr[0]['suite']['connections'])
 
-        self.persist_output(template_output)
+        try:
+            if self.yaml_data['fixture'] is not None:
+                self.fixture = self.yaml_data['fixture']
+                self.template_output = j2_env.get_template("testfixture.jj2")\
+                    .render(create_date=strftime("%a, %d %b %Y %X +0000", gmtime()),
+                            fixture=self.fixture)
+        except KeyError:
+            self.fixture = "unittest.TestCase"  # Default value for fixture
+            self.log.info("Fixture not present, generating TestSuite...")
+        finally:
+            self.template_output = j2_env.get_template("testsuite.jj2") \
+                .render(create_date=strftime("%a, %d %b %Y %X +0000", gmtime()),
+                        fixture=self.fixture,
+                        tests=self.yaml_data['tests'])
+
+        self.persist_output(self.template_output)
 
     def persist_output(self, output):
-        import os
         # TODO: Decide if naming the files based on the test name from the yaml is ok
-        with open("%s/%s%s" % (self.out_dir, self.arr[0]['suite']['name'], self.file_ext), 'w') as f:
-            # TODO: Figure out how to dynamically write the shabang based on file ext...
-            # Should probably be a header variable populated based on the file_ext
-            f.write("#!/bin/sh\n")
-
+        with open("%s/%s%s" % (self.out_dir, str(self.yaml_data['name']).replace(' ', ''), self.file_ext), 'w') as f:
             os.chmod(f.name, 0770)
 
             f.write(output)
