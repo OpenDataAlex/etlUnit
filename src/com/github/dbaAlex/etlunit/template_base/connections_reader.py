af350cc4879ca3d1ad29f256b451f4a3b39bf57c
coty <sutherland.coty@gmail.com>
2013-08-21 16:23:39 -0400

    Updated resource file location.

--- src/com/github/dbaAlex/etlunit/template_base/connections_reader.py
+++ src/com/github/dbaAlex/etlunit/template_base/connections_reader.py
@@ -6,7 +6,7 @@ import yaml
 connections = {}
 
 try:
-    filename = '/home/coty/custom/source/etlUnit/res/jinja2/connections.yml'
+    filename = '/home/coty/custom/source/etlUnit/res/connections.yml'
     with open(filename, 'r') as f:
         prop_list = yaml.load(f.read())
         connections = prop_list
