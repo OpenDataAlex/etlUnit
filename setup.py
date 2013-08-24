87fc511a02b5532936c811fb45f120a92a17d0db
coty <sutherland.coty@gmail.com>
2013-08-24 13:45:14 -0400

    Updated requirements and structure of setup.py a bit.

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ __author__ = 'coty'
 
 from setuptools import setup, find_packages
 
-setup(name='etlUnit',
-      packages=find_packages(),
-      version='0.1',
-      description='Test framework for ETL code.',
-      author='Alex Meadows, Coty Sutherland',
-      install_requires=[
-          'pyyaml',
-          'jinja2',
-          'sqlalchemy'
-      ],
+setup(
+    name='etlUnit',
+    version=etlunit.__version__,
+    url='http://github.com/dbaAlex/etlunit',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    description='Test framework for ETL code.',
+    author='Alex Meadows, Coty Sutherland',
+    packages=find_packages(),
+    install_requires=[
+        'pyyaml',
+        'jinja2',
+        'sqlalchemy'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta'
+        'Natural Language :: English',
+    ]
 )
