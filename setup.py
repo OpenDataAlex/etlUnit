d82aa158e4852825a9ee9c111c3b1e97a54a4c5e
coty <sutherland.coty@gmail.com>
2013-08-27 10:21:39 -0400

    Updated setup.py to read the requirements file for install_requires

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ __author__ = 'coty'
 
 from setuptools import setup, find_packages
 import etlunit
-
-
 from setuptools.command.test import test as TestCommand
 import sys
 
@@ -18,6 +16,11 @@ class Tox(TestCommand):
         errcode = tox.cmdline(self.test_args)
         sys.exit(errcode)
 
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='etlUnit',
     version=etlunit.__version__,
@@ -26,21 +29,12 @@ setup(
     description='Test framework for ETL code.',
     author='Alex Meadows, Coty Sutherland',
     packages=find_packages(),
-    install_requires=[
-        'Jinja2==2.7.1',
-        'MarkupSafe==0.18',
-        'MySQL-python==1.2.4',
-        'PyYAML==3.10',
-        'SQLAlchemy==0.8.2',
-        'unittest-xml-reporting==1.5.0',
-        'wsgiref==0.1.2',
-    ],
+    install_requires=required,
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 4 - Beta'
         'Natural Language :: English',
     ],
-    #test_requires=['tox'],
     cmdclass = {'tox': Tox},
     test_suite='etlunit.test'
 )
