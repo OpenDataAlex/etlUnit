39d772fc5646914ed27c4077c33c78d4a7a75c76
coty <sutherland.coty@gmail.com>
2013-08-28 13:59:09 -0400

    Basic refactoring. Changed some variable names to make the code more readable.

--- etlunit/code_generator.py
+++ etlunit/code_generator.py
@@ -10,7 +10,7 @@ import os
 from etlunit.utils.settings import etlunit_config, console
 
 
-class CodeGenerator():
+class CodeGenerator:
     """
         This class performs the generation of the code. Using the Jinja2 template engine, we are taking in YAML
         and generating code from it by filling in templates.
@@ -19,7 +19,7 @@ class CodeGenerator():
     # TODO: Determine if the array passed into the class is a single YAML array or if it is multile arrays from files
     def __init__(self, out_dir, data):
         """
-            This method initializes the logging variables as well as the yaml_data and out_dir variables.
+            This method initializes the logging variables as well as the resource_data and out_dir variables.
             :param out_dir: The output directory that we will generate code in.
             :type out_dir: str.
             :param data: An array of data that comes from the YAML resource file.
@@ -29,7 +29,7 @@ class CodeGenerator():
         self.log.setLevel(etlunit_config['logging_level'])
         self.log.addHandler(console)
 
-        self.yaml_data = data
+        self.resource_data = data
         self.out_dir = out_dir
 
     def generateCode(self, test):
@@ -39,16 +39,11 @@ class CodeGenerator():
             the code that we generate, it prints to stdout instead.
             :type test: bool.
         """
-        #TODO: Maybe we should have a YAML validation class?
-        #Totaly agree - that makes perfect sense.
-        from jinja2 import Environment, FileSystemLoader
-        from time import strftime, gmtime
+        # TODO: Implement a YAML validation class.
 
-        # TODO: Find a more efficient way to pull in this template other than ../
-        # Is is possible to parameterize the template directory?  It should be a static location... - Alex
-        # Maybe we can use the PackageLoader
-        out_path = "%s/../templates/" % os.path.dirname(os.path.abspath(__file__))
-        j2_env = Environment(loader=FileSystemLoader(out_path), trim_blocks=True, lstrip_blocks=True)
+        from jinja2 import Environment, PackageLoader
+        from time import strftime, gmtime
+        j2_env = Environment(loader=PackageLoader('etlunit', 'templates'), trim_blocks=True, lstrip_blocks=True)
 
         # Header lines created here and added to the templates as required
         header = "#!/usr/bin/python\n" \
@@ -58,17 +53,17 @@ class CodeGenerator():
                  "#\n" % \
                  strftime("%a, %d %b %Y %X +0000", gmtime())
 
-        for yml in self.yaml_data.keys():
-            self.log.info("Generating code from %s..." % yml)
-            self.yml_data = self.yaml_data[yml]
+        for test_suite_name in self.resource_data.keys():
+            self.log.info("Generating code from %s..." % test_suite_name)
+            self.test_suite = self.resource_data[test_suite_name]
 
             # TODO: Determine how we handle dependencies on single files.
             # TODO: Added fixture definition to the mix. Currently it generates a fixture but it has no variables.
             try:
-                if self.yml_data['fixture'] is not None:
+                if self.test_suite['fixture'] is not None:
                     from etlunit.yaml_reader import YAMLReader
-                    self.fixture = self.yml_data['fixture']
-                    fixture_res = "../res/%s.yml" % self.fixture
+                    self.fixture = self.test_suite['fixture']
+                    fixture_res = "../res/%s.test_suite_name" % self.fixture
                     reader = YAMLReader(fixture_res, None)
                     fixture_data = reader.readTests()[fixture_res]
 
@@ -78,7 +73,7 @@ class CodeGenerator():
                                 setup=fixture_data['setup'],
                                 teardown=fixture_data['teardown'])
 
-                    self.persist_output(self.yml_data['fixture'], self.template_output, test)
+                    self.persist_output(self.test_suite['fixture'], self.template_output, test)
             except KeyError:
                 self.fixture = "unittest.TestCase"  # Default value for fixture
                 self.log.info("Fixture not present, generating TestSuite...")
@@ -86,10 +81,10 @@ class CodeGenerator():
                 self.template_output = j2_env.get_template("testsuite.jj2") \
                     .render(header=header,
                             fixture=self.fixture,
-                            tests=self.yml_data['tests'],
-                            suitename=self.yml_data['name'].replace(' ', ''))
+                            tests=self.test_suite['tests'],
+                            suitename=self.test_suite['name'].replace(' ', ''))
 
-                self.persist_output(self.yml_data['name'], self.template_output, test)
+                self.persist_output(self.test_suite['name'], self.template_output, test)
         self.log.info("Code generation complete.")
 
     def persist_output(self, name, output, test):
