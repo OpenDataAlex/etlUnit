f3cd8dedbbac523fd459a4f83423c19f52f6cb7d
coty <sutherland.coty@gmail.com>
2013-09-03 15:46:30 -0400

    Created a test stub. Eventually itll be a real test...

---
+++ etlunit/test/test_yaml_reader.py
@@ -0,0 +1,7 @@
+"""
+    This module is a place holder for the yaml_reader tests.
+
+    All this test would do is prove that we can read in a file (or files) into YAML. Not sure it is required.
+"""
+
+__author__ = 'coty'
